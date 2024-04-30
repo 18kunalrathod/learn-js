adabff169bbf1af1a5559ad10b1cd3bf379a2576
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-04-30 09:19:17 +0000

    container config done

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,24 @@
+
+const accountId = 156785; //Declaring constant
+let  accountEmail = "javascript@gmail.com"; //Declaring block varible
+var accountPassword = "12345";// Declaring Variable
+accountCity = "Bangalore"; // we can resever variable value without declaring variable but it is not a good pratice
+let accountState ; // op will be undefined because in JS if we decalare a variable and keep it empty then it will give us undefined
+
+
+// accountId  = 2 // not allowed because we have declared it const 
+
+
+accountEmail = "abcd@gmail.com"
+accountPassword  = "21456"
+accountCity = "USA"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+var is not used because of issue in block scope and function scope
+*/
+
+
