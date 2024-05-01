ff266e428c72720d14545cd3be12f866657c69ab
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-01 07:54:54 +0000

    conversion of data types

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "abc"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "50" => 33
+//"50abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = "asd";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false;
+// "asd" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
