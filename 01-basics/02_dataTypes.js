ac9e30e39950f7f18564da08b89f6ed9e40f644b
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-01 05:45:24 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 4); we are using node.js, not in browers
+
+//code readability should be high
+
+console.log(3 + 3);
+console.log("JavaScript")
+
+//data type
+let name = "Kunal" // string
+let age = 27 // number
+let isLoggedIn = false // booleaan (true/false)
+// bigint
+//null => standalone value
+let state; //undefined
+//symbol
+
+
+
+//object 
+console.log(typeof(null)) // will get the output object
+console.log(typeof(undefined)) // will get the output undefined
