8f12905cc6b6b4da4832391bcaacccdb45570ec1
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-02 12:17:33 +0000

    comparison operators

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log( 2 > 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 < 1 );
+// console.log( 2 == 1 );
+// console.log( 2 != 1 );
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log( null > 0)
+console.log( null < 0)
+console.log( null == 0)
+console.log( null >= 0) // comparison covert null to a number, treating it as 0, thats why null >= 0 is true and null > 0 us false
+
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// strict check === 
+
+console.log("2" == 2);
+console.log("2" === 2);
