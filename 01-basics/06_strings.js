113e5990e657f0d2a3f57ebe9b5759564d74f4a9
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-02 17:23:14 +0000

    strings

---
+++ 01-basics/06_strings.js
@@ -0,0 +1,8 @@
+const name = "kunal";
+
+const repoCount = 50;
+
+// console.log(name + repoCount) 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //string interpolation
+
