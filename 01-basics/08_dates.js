75618ead8af992f6617c9bd034582e403d9d94f7
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-04 12:24:39 +0000

    dates

---
+++ 01-basics/08_dates.js
@@ -0,0 +1,20 @@
+//dates
+
+let date = new Date()  // date is object
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toDateString())
+
+
+// let  myCreateDate = new Date(2024, 0, 24)
+// let  myCreateDate = new Date("2024-05-04") // yymmdd
+let  myCreateDate = new Date("01-04-2024") // ddmmyy
+
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreateDate.getTime() )
+
+// console.log(Math.floor((Date.now()/1000)));
