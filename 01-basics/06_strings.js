18719456b54d66d11da19e0cce118a3ae81044df
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-04 09:12:30 +0000

    strings

--- 01-basics/06_strings.js
+++ 01-basics/06_strings.js
@@ -4,5 +4,31 @@ const repoCount = 50;
 
 // console.log(name + repoCount) 
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //string interpolation
 
+const gameName = new String('stephano-dr')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('e'));
+
+const newStr = gameName.substring(0, 4) // include 0 index to 3rd index does not include 4th index
+console.log(newStr)
+
+const anotherStr = gameName.slice(-8, 4)
+console.log(anotherStr)
+
+const newStrOne = "  kunal  "
+console.log(newStrOne)
+console.log(newStrOne.trim()) // works on white spaces
+
+
+const url = "https://kunal.com/kunal%20rathod"
+console.log(url.replace('%20', '-')) // replace
+console.log(url.includes('kunal')) // shows the keyword which is present or not
+
+console.log(gameName.split('-'))
