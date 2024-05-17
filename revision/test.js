910fc7c3bbe8b82de0e65c4a1eabc9ce7bdb2f48
18kunalrathod <165719594+18kunalrathod@users.noreply.github.com>
2024-05-17 05:27:44 +0000

    test

---
+++ revision/test.js
@@ -0,0 +1,6 @@
+// var let const are the variable of the javascript
+// these are used to store the data
+
+//data type - sttring, number, boolean, bigInt, null, undefined, symbol
+// these are also primitive type data types
+//non primitives = array , obj , functions
